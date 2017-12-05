f23e45109d7970834b4472de0eee1fbf819034d1
Nicolas <nabitante@fecliba.org.ar>
2017-12-05 11:32:45 -0300

    Se crea servicio de login simple

--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,24 @@
-const mysql = require('mysql');
 
+const Connection = require('../utils/connection');
 
-connection  = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'nico0208',
-        database: 'dbapp'
-    }
-);
+ conecction =Connection.getConection();
+
+// connection  = mysql.createConnection(
+//     {
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'nico0208',
+//         database: 'dbapp'
+//     }
+// );
 
 let userModel = {};
 
 
-userModel.getUsers = (callback) =>{
+userModel.login = (data,callback) =>{
     if (connection){
-        connection.query('SELECT * FROM users ORDER BY id', (err, rows) => {
+        sql = `SELECT * FROM users WHERE username =${connection.escape(data.username)} AND password = ${connection.escape(data.password)} ORDER BY id`;
+        connection.query(sql, (err, rows) => {
             if(err){
                 throw err;
             }else callback(null, rows);
@@ -24,52 +27,5 @@ userModel.getUsers = (callback) =>{
 
 };
 
-userModel.createUser = (userData, callback) => {
-    if(connection){
-        connection.query('INSERT INTO users SET ?', userData, (err, result) =>{
-            if(err){
-                throw err;
-            }else {
-                callback(null,  {'insertId':result.insertId})
-            }
-        })
-    }
 
-};
-userModel.updateUser = (userData, callback) => {
-    if(connection){
-        const sql =`
-            UPDATE users SET 
-            username = ${connection.escape(userData.username)},
-            password = ${connection.escape(userData.password)},
-            name = ${connection.escape(userData.name)} 
-            WHERE id = ${connection.escape(userData.id)}
-        `;
-        connection.query(sql,(err, resultado) =>{
-            if(err){
-                throw err;
-            }else {
-                callback(null, {
-                        "msg": "Success"
-                    })
-            }
-        })
-    }
-
-};
-userModel.deleteUser = (id, callback) =>{
-  if(connection){
-      const sql =`DELETE FROM users WHERE id= ${connection.escape(id)}`;
-      connection.query(sql,(err, resultado) => {
-          if (err){
-              throw err;
-          }else {
-              callback(null, {
-                  "msg":"success"
-              })
-          }
-      });
-
-  }
-};
 module.exports = userModel;
